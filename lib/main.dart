384a36a287ce8fb979629a5f6d18a20808932605
WesleyAugusto <ranzerap@gmail.com>
2022-05-15 20:24:39 -0300

    usuario e senha

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp(MaterialApp(
-    title: "Frases do dia ",
-    home: Column(children: <Widget> [
-      Text("usuario:"),
-      Text("senha:"),
-    ],),
-  ));
-
-
+void main() {
+  runApp(
+    MaterialApp(
+        title: "Frases do dia ",
+        home: Container(
+          color: Colors.black,
+          child: Row(
+            children: [Column(         children: <Widget>[
+              Text("usuario:"),
+              Text("senha:"),
+            ],),
+            ],
+          ),
+        )),
+  );
 }
