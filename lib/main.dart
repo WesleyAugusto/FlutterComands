21ed99adc858e090f22fccd10dd2155a96202c10
WesleyAugusto <ranzerap@gmail.com>
2022-05-16 21:53:02 -0300

    criando botão

--- lib/main.dart
+++ lib/main.dart
@@ -7,31 +7,15 @@ void main() {
       home: Container(
         color: Colors.white,
         child: Column(children: <Widget>[
-          Text("wesley augusto polatto lindo",
-          style: TextStyle(
-              fontSize: 100,
-            fontStyle: FontStyle.italic,
-            fontWeight: FontWeight.w100,
-            color: Colors.green,
-              decoration: TextDecoration.underline,
-              decorationColor: Colors.redAccent,
-              decorationStyle: TextDecorationStyle.wavy,
-              wordSpacing: 5,
-              letterSpacing: 5.1,
-            shadows: <Shadow>[
-              Shadow(
-                offset: Offset(10.0, 10.0),
-                blurRadius: 3.0,
-                color: Color.fromARGB(255, 0, 0, 0),
-              ),
-              Shadow(
-                offset: Offset(10.0, 10.0),
-                blurRadius: 8.0,
-                color: Color.fromARGB(125, 59, 191, 93),
-              ),
-            ],
-
-          )),
+          FlatButton(onPressed: (){
+            print("botão precionado");
+           }   ,child: Text(
+              "clique aqui",
+            style: TextStyle(
+              fontSize: 20,
+              color: Colors.black,
+            decoration: TextDecoration.none)
+          ))
         ],)
           )
       ));
