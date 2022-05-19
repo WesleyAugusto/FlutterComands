754de570545cda962fa4db628ca13750945b743b
WesleyAugusto <ranzerap@gmail.com>
2022-05-18 23:00:35 -0300

    espaçamentos

--- lib/main.dart
+++ lib/main.dart
@@ -3,19 +3,24 @@ import 'package:flutter/painting.dart';
 
 void main() {
   runApp(MaterialApp(
+    //debugShowCheckedModeBanner: false,//remove Debug
       title: "Frases do dia ",
       home: Container(
-        color: Colors.white,
+        //color: Colors.white, //cor de  fundo
+        padding: EdgeInsets.fromLTRB(40, 40, 20, 20),
+        margin: EdgeInsets.all(45),
+        decoration: BoxDecoration(
+          border: Border.all(width: 80,color: Colors.black)
+        ),
         child: Column(children: <Widget>[
-          FlatButton(onPressed: (){
-            print("botão precionado");
-           }   ,child: Text(
-              "clique aqui",
-            style: TextStyle(
-              fontSize: 20,
-              color: Colors.black,
-            decoration: TextDecoration.none)
-          ))
+          Text("t1"),
+          Padding(padding: EdgeInsets.all(30)),
+          Text("t1"),
+
+         /*Text(
+             "texto da aula do professor testando o fundo e que a ldado nao tem limite ",
+                 textAlign: TextAlign.justify
+         )*/
         ],)
           )
       ));
