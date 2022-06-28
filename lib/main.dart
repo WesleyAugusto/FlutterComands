63ca126c960fb8b858818b30c2aada84e2e94131
WesleyAugusto <ranzerap@gmail.com>
2022-06-28 14:34:18 -0300

    Statefull,Stateless

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,78 @@
 import 'package:flutter/material.dart';
 
+/*
+  Stateless -> Widgets que não podem ser alterados (contantes)
+  Stateful -> Widgets que podem ser alterados (variáveis)
+ */
+
 void main(){
 
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: Scaffold(
+    home: HomeStateful(),
+  ));
+
+}
+
+/*
+
+var _texto = "Jamilton Damasceno";
+
+
+var _texto = "Curso Flutter";
+
+ */
+
+class HomeStateful extends StatefulWidget {
+  @override
+  _HomeStatefulState createState() => _HomeStatefulState();
+}
+
+class _HomeStatefulState extends State<HomeStateful> {
+
+  var _texto = "Jamilton Damasceno";
+
+  @override
+  Widget build(BuildContext context) {
+
+    print("Build chamado");
+
+    return Scaffold(
       appBar: AppBar(
-        title: Text("Instagram"),
+        title: Text( "Instagram" ),
+        backgroundColor: Colors.green,
+      ),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            RaisedButton(
+              onPressed: (){
+
+                setState(() {
+                  _texto = "Curso Flutter";
+                });
+              },
+              child: Text("Clique aqui"),
+            ),
+            Text("Nome: $_texto ")
+          ],
+        ),
+      ),
+
+    );
+  }
+}
+
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+    var _titulo = "Instagram";
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text( _titulo ),
         backgroundColor: Colors.green,
       ),
       body: Padding(
@@ -25,7 +91,7 @@ void main(){
           ),
         ),
       ),
-    ),
-  ));
+    );
 
+  }
 }
